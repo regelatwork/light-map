120314e48f982d2beb64db9f05d48d3042404238
Rodrigo Chandia <rodrigo.chandia@gmail.com>
2026-03-08 23:55:44 -0400

    chore(frontend): add linting, formatting, and gitignore

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,6 @@ function App() {
     <div className="bg-blue-500 text-white p-4">
       <h1 className="text-2xl font-bold">Tailwind is Active</h1>
     </div>
-  )
+  );
 }
-export default App
+export default App;
